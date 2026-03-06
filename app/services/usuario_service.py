5c981c40fb466aa492d23448fba0d60992a78537
Anibal Moreno Martin <anibal.moreno@issec.internal>
2026-03-06 14:43:12 +0000

    aprendiendo testing basico

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -8,8 +8,8 @@ def create_usuario_service(usuario: Usuario, session: Session, repo=usuario_repo
     return repo.add(session, usuario)
 
 
-def get_usuario_service(usuario_id: int, session: Session):
-    usuario = usuario_repo.get(session, usuario_id)
+def get_usuario_service(usuario_id: int, session: Session, repo=usuario_repo):
+    usuario = repo.get(session, usuario_id)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
@@ -19,21 +19,21 @@ def list_usuarios_service(session: Session, repo=usuario_repo):
     return repo.list_all(session)
 
 
-def update_usuario_service(usuario_id: int, usuario_data: Usuario, session: Session):
-    usuario = usuario_repo.get(session, usuario_id)
+def update_usuario_service(usuario_id: int, usuario_data: Usuario, session: Session, repo=usuario_repo):
+    usuario = repo.get(session, usuario_id)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
     usuario.nombre = usuario_data.nombre
     usuario.username = usuario_data.username
 
-    return usuario_repo.update(session, usuario)
+    return repo.update(session, usuario)
 
 
-def delete_usuario_service(usuario_id: int, session: Session):
-    usuario = usuario_repo.get(session, usuario_id)
+def delete_usuario_service(usuario_id: int, session: Session, repo=usuario_repo):
+    usuario = repo.get(session, usuario_id)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
-    usuario_repo.delete(session, usuario)
+    repo.delete(session, usuario)
     return {"ok": True}
